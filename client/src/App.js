98d6c6cf63a65b0aae9bd6f5ac16fe22aea7ffc5
Jeezman <tobiadeyemi91@gmail.com>
2022-03-26 11:13:24 +0100

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import styled from 'styled-components'
+import { LoginForm } from './Login/LoginForm';
+import { Loader } from './components/Loader';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
 
 export default App;
+
+
+const Login = ({ }) => {
+  return <LoginContainer>
+    <LoginForm />
+  </LoginContainer>
+}
+
+const LoginContainer = styled.main`
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
